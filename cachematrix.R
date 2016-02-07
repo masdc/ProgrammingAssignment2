b733f2f82fe0d99144ae10978ad77b0d022bf733
Millan Sanchez <masdc@hotmail.com>
2016-02-06 23:21:32 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+
+
+## makeCacheMatrix stores a matrix X in memory
+## cacheSolve: Function that shows the inverse of a matriz if it already calculated...if not it evaluates the inverse
+
+## makeCacheMatrix uses scoping rules and stores matrices in memory
 makeCacheMatrix <- function(x = numeric()) {
         
         
@@ -12,7 +18,7 @@ makeCacheMatrix <- function(x = numeric()) {
                 x
         }
         cacheInverse <- function(solve) {
-                cache <<- solve
+                cache <<- solve #solve the inverse of the matrix
         }
         getInverse <- function() {
                 cache
@@ -21,18 +27,19 @@ makeCacheMatrix <- function(x = numeric()) {
         list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
-
+## cacheSolve: Function that shows the inverse of a matriz if it already calculated...if not it evaluates the inverse
 cacheSolve <- function(y, ...) {
       
         inverse <- y$getInverse()
   
-        if(!is.null(inverse)) {
+        if(!is.null(inverse)) # evaluate if the inverse is already calculated and in memory
+        { 
                 message("Obtaining cached data")
-                return(inverse)
+                return(inverse) 
         }
       
         data <- y$getMatrix()
-        inverse <- solve(data)
-        y$cacheInverse(inverse)
+        inverse <- solve(data) # calculte the inverse of the matrix
+        y$cacheInverse(inverse) #cache the inverse of the matrix
         inverse
 }
