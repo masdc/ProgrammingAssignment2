32090552eb5955e23cde929cddd52ba0b5b3aa55
Millan Sanchez <masdc@hotmail.com>
2016-02-06 22:51:18 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -25,12 +25,12 @@ cacheSolve <- function(X, ...)
 if(require("corpcor")){
 	print("Corpcor instalado")
 	} else {
-		print("ntentando instalar corpcor")
+		print("intentando instalar corpcor")
 		install.packages("corpcor")
 		if(require(corpcor)){
 			print("corpcor instalado y cargado")
 			} else {
-			stop("mo pudo intalarse corpcor")
+			stop("no pudo intalarse corpcor")
 			}
 		}
 inverse <- X$getinverse()
@@ -45,37 +45,3 @@ X$setinverse(inverse)
 inverse
 }
 
-
-#Experiment to try if it works
-#square matrix
-X <- matrix(rpois(25,3), nrow = 5)
-cX <- makeCacheMatrix(X)
-cX$get()
-cacheSolve(cX)
-cacheSolve(cX)
-invX <- cacheSolve(cX)
-
-#Experiment to try if it works
-#rectangular matrix rows > cols
-Y <- matrix(rpois(20,2), nrow = 5, ncol = 4)
-cY <- makeCacheMatrix(Y)
-cY$get()
-cacheSolve(cY)
-cacheSolve(cY)
-invY <- cacheSolve(cY)
-
-#Experiment to try if it works
-#rectangular matrix rows < cols
-Z <- matrix(rpois(20,1), nrow = 4, ncol = 5)
-cZ <- makeCacheMatrix(Z)
-cZ$get()
-cacheSolve(cZ)
-cacheSolve(cZ)
-invZ <- cacheSolve(cZ)
-
-#Experiment to try if it works
-#multiplication must return identity or closer
-invX %*% X 
-X %*% invX
-invY %*% Y 
-Z %*% invZ 
