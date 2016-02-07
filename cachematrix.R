a41c4aa94255f2650a138e294add3799c4d9a900
Millan Sanchez <masdc@hotmail.com>
2016-02-06 22:54:32 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-
-## Write a short comment describing this function
-
-
-makeCacheMatrix <- function(X = matrix()) {
-inverse <- NULL
-set <- function(Y){
-	X <<- Y
-	inverse <<- NULL
-	}
-get <- function() X
-setinverse <- function(Inverse) inverse <<- Inverse
-getinverse <- function() inverse
-list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+makeCacheMatrix <- function(x = numeric()) {
+        
+        
+        cache <- NULL
+        
+       
+        setMatrix <- function(newValue) {
+                x <<- newValue
+                cache <<- NULL
+        }
+        getMatrix <- function() {
+                x
+        }
+        cacheInverse <- function(solve) {
+                cache <<- solve
+        }
+        getInverse <- function() {
+                cache
+        }
+        
+        list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
-## Write a short comment describing this function
 
-
-cacheSolve <- function(X, ...) 
-{
-if(require("corpcor")){
-	print("Corpcor instalado")
-	} else {
-		print("intentando instalar corpcor")
-		install.packages("corpcor")
-		if(require(corpcor)){
-			print("corpcor instalado y cargado")
-			} else {
-			stop("no pudo intalarse corpcor")
-			}
-		}
-inverse <- X$getinverse()
-if(!is.null(inverse)){
-	message("matriz está en memoria")
-	return(inverse)
-	}
-message("la inversa no está en memoria entonces va a ser calculada")
-data <- X$get()
-inverse <- pseudoinverse(data, ...)
-X$setinverse(inverse)
-inverse
+cacheSolve <- function(y, ...) {
+      
+        inverse <- y$getInverse()
+  
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+      
+        data <- y$getMatrix()
+        inverse <- solve(data)
+        y$cacheInverse(inverse)
+        inverse
 }
-
